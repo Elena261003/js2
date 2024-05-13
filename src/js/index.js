fa87425aded58c69f87e29d46813d20e9812460b
monre036 <samsonova.lena.03@gmail.com>
2024-05-13 18:54:37 +0200

    ggg

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,51 @@ function wordToUpper(word) {
   }
 }
 wordToUpper('Hola');
+
+function wordToUper(word) {
+  word.length > 5
+    ? console.log(word.toUpperCase())
+    : console.log(word.toLowerCase());
+}
+
+wordToUper('Hellooooo');
+
+function makeConjugation(verbo1, verbo2) {
+  if (verbo1.endsWith('ar') && verbo2.endsWith('ar')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la primera conjugación`
+    );
+  } else if (verbo1.endsWith('er') && verbo2.endsWith('er')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la segunda conjugación`
+    );
+  } else if (verbo1.endsWith('ir') && verbo2.endsWith('ir')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la tercera conjugación`
+    );
+  } else if (verbo1.endsWith('ar') && verbo2.endsWith('er')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la primera conjugación`
+    );
+  } else if (verbo1.endsWith('er') && verbo2.endsWith('ir')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la segunda conjugación`
+    );
+  } else if (verbo1.endsWith('ir') && verbo2.endsWith('ar')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la tercera conjugación`
+    );
+  } else if (verbo1.endsWith('ar') && verbo2.endsWith('ir')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la primera conjugación`
+    );
+  } else if (verbo1.endsWith('er') && verbo2.endsWith('ar')) {
+    console.log(
+      `Los verbos ${verbo1} y ${verbo2} son de la segunda conjugación`
+    );
+  } else verbo1.endsWith('ir') && verbo2.endsWith('er');
+  console.log(`Los verbos ${verbo1} y ${verbo2} son de la tercera conjugación`);
+}
+
+makeConjugation('llorar', 'comer');
+makeConjugation('llamar', 'morir');
