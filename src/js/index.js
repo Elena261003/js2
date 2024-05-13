c5db51717cd23f67988743eea235d02ca10205d7
monre036 <samsonova.lena.03@gmail.com>
2024-05-13 18:12:22 +0200

    ggg

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+function wordToUpper(word) {
+  if (word.length > 5) {
+    console.log(word.toUpperCase());
+  } else {
+    console.log(word.toLowerCase());
+  }
+}
+wordToUpper('Hola');
